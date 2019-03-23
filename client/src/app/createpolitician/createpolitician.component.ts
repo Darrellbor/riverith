229581335e05060e21141ca926965879e6bacd4a
Oluwatomisin Gbotolorun <tosingbotolorun@yahoo.co.uk>
2019-03-23 07:19:32 +0100

    Fragment of the implementation of the front end

---
+++ client/src/app/createpolitician/createpolitician.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-createpolitician',
+  templateUrl: './createpolitician.component.html',
+  styleUrls: ['./createpolitician.component.scss']
+})
+export class CreatepoliticianComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
